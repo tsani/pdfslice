ffefe1e829490c5f46345859e68321966465a6f0
Jacob Errington <git@mail.jerrington.me>
2025-03-13 13:49:46 -0400

    refactor; add join operation

--- pdfslice.py
+++ pdfslice.py
@@ -4,22 +4,32 @@ import pypdf
 import sys
 import argparse
 
-def slice(args):
-    reader = pypdf.PdfReader(args.SRC)
-    writer = pypdf.PdfWriter()
-    writer.append(fileobj=reader, pages=(args.START_PAGE, args.END_PAGE))
-    writer.write(args.DEST)
-    writer.close()
+class Action:
+    @staticmethod
+    def slice(args):
+        reader = pypdf.PdfReader(args.SRC)
+        writer = pypdf.PdfWriter()
+        writer.append(fileobj=reader, pages=(args.START_PAGE, args.END_PAGE))
+        writer.write(args.DEST)
+        writer.close()
 
-def dup(args):
-    reader = pypdf.PdfReader(args.SRC)
-    writer = pypdf.PdfWriter()
-    for _ in range(args.COPIES):
-        writer.append(fileobj=reader)
-    writer.write(args.DEST)
-    writer.close()
+    @staticmethod
+    def dup(args):
+        reader = pypdf.PdfReader(args.SRC)
+        writer = pypdf.PdfWriter()
+        for _ in range(args.COPIES):
+            writer.append(fileobj=reader)
+        writer.write(args.DEST)
+        writer.close()
 
-ACTIONS = { 'slice': slice, 'dup': dup }
+    @staticmethod
+    def join(args):
+        writer = pypdf.PdfWriter()
+        for in_path in args.PATHS:
+            reader = pypdf.PdfReader(in_path)
+            writer.append(fileobj=reader)
+        writer.write(args.DEST)
+        writer.close()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -35,5 +45,10 @@ if __name__ == '__main__':
     dup_parser.add_argument("DEST")
     dup_parser.add_argument("SRC")
     dup_parser.add_argument("COPIES", type=int)
+
+    join_parser = subparsers.add_parser("join", help="concatenate PDFs")
+    join_parser.add_argument('DEST')
+    join_parser.add_argument('PATHS', type=str, nargs='+')
+
     args = parser.parse_args()
-    ACTIONS[args.command](args)
+    getattr(Action, args.command)(args)
